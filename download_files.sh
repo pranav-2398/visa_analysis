cad3a5eef20dfdc13ff771f040c79da3447dc56a
pranav-2398 <76903645+pranav-2398@users.noreply.github.com>
2024-03-02 23:34:02 +0530

    Initial Commit

---
+++ download_files.sh
@@ -0,0 +1,1 @@
+scp -i spark-clusters_key.pem -rp azureuser@20.193.141.186:/home/azureuser/output/* src/output/
---
+++ upload_files.sh
@@ -0,0 +1,1 @@
+scp -i spark-clusters_key.pem -r ./src/* azureuser@20.193.141.186:/home/azureuser
